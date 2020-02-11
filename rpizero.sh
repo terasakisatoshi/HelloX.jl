423e40463a4b37c3dc01bf59af693df90301a911
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-02-12 03:17:28 +0900

    update

--- rpizero.sh
+++ rpizero.sh
@@ -10,4 +10,5 @@ docker run --rm -it --name versioncheck -v ${PWD}:/work -w /work ${JL_RPIZERO} j
 # Build executable which will be stored under a directory named `build`
 docker run --rm -it --name buildrpizero -v ${PWD}:/work -w /work ${JL_RPIZERO} make rpizero
 # Test to run binary on other environments that does not have Julia environment
-docker run --rm -it -v ${PWD}:/work -w /work balenalib/raspberry-pi:buster-20191030 build/bin/HelloX
+# This will fail, but could run on my (real) Raspberry Pi Zero W.
+# docker run --rm -it -v ${PWD}:/work -w /work balenalib/raspberry-pi:buster-20191030 build/bin/HelloX
